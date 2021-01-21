3a6e069d334a9987d853b0a7bda277bddca875ba
Tamara Kalachova <Lipskaya.tamara@gmail.com>
2021-01-21 12:55:05 +0200

    TestNG anotations

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -2,21 +2,22 @@ package org.example;
 
 import org.example.driver.Browser;
 import org.example.util.TestListener;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 @Listeners({TestListener.class})
 public class BaseTest {
     private static Browser browser;
 
-    @BeforeAll//открывает браузер перед выполнением сценария
+    @BeforeSuite//открывает браузер перед выполнением сценария
     public static void setup() {
         browser = Browser.getInstance();
     }
 
-    @AfterAll
+    @AfterSuite
     public static void tearDown() {
+
         browser.stopBrowser();
     }
 }
